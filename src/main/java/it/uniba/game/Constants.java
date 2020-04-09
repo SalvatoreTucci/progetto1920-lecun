048a54d297379cde46538003d713115007635581
Domenico Dell'Olio <zeito99@gmail.com>
2020-04-09 18:44:09 +0200

    Resa pubblica la classe Constants

--- src/main/java/it/uniba/game/Constants.java
+++ src/main/java/it/uniba/game/Constants.java
@@ -2,21 +2,21 @@ package it.uniba.game;
 
 
 
-final class Constants {
+public final class Constants {
 	/*
 	 * Class containing the constant values used in the application
 	 */
-	final static String W_KING = "♔";
-	final static String B_KING = "♚";
-	final static String W_QUEEN = "♕";
-	final static String B_QUEEN = "♛";
-	final static String W_BISHOP = "♗";
-	final static String B_BISHOP = "♝";
-	final static String W_ROOK = "♖";
-	final static String B_ROOK = "♜";
-	final static String W_KNIGHT = "♘";
-	final static String B_KNIGHT = "♞";
-	final static String W_PAWN = "♙";
-	final static String B_PAWN = "♟";
+	public final static String W_KING = "♔";
+	public final static String B_KING = "♚";
+	public final static String W_QUEEN = "♕";
+	public final static String B_QUEEN = "♛";
+	public final static String W_BISHOP = "♗";
+	public final static String B_BISHOP = "♝";
+	public final static String W_ROOK = "♖";
+	public final static String B_ROOK = "♜";
+	public final static String W_KNIGHT = "♘";
+	public final static String B_KNIGHT = "♞";
+	public final static String W_PAWN = "♙";
+	public final static String B_PAWN = "♟";
 	
 }
