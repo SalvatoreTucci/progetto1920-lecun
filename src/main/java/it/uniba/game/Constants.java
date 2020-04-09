a4bef8e10111e967ed1469fc07eff398b138a2b2
Raffo99 <dis.raffaele99@gmail.com>
2020-04-09 19:09:38 +0200

    Aggiunte le costanti che servono alla classe Pawn

--- src/main/java/it/uniba/game/Constants.java
+++ src/main/java/it/uniba/game/Constants.java
@@ -6,17 +6,26 @@ public final class Constants {
 	/*
 	 * Class containing the constant values used in the application
 	 */
-	public final static String W_KING = "♔";
-	public final static String B_KING = "♚";
-	public final static String W_QUEEN = "♕";
-	public final static String B_QUEEN = "♛";
-	public final static String W_BISHOP = "♗";
-	public final static String B_BISHOP = "♝";
-	public final static String W_ROOK = "♖";
-	public final static String B_ROOK = "♜";
-	public final static String W_KNIGHT = "♘";
-	public final static String B_KNIGHT = "♞";
-	public final static String W_PAWN = "♙";
-	public final static String B_PAWN = "♟";
+	public final static String W_KING = "â™”";
+	public final static String B_KING = "â™š";
+	public final static String W_QUEEN = "â™•";
+	public final static String B_QUEEN = "â™›";
+	public final static String W_BISHOP = "â™—";
+	public final static String B_BISHOP = "â™�";
+	public final static String W_ROOK = "â™–";
+	public final static String B_ROOK = "â™œ";
+	public final static String W_KNIGHT = "â™˜";
+	public final static String B_KNIGHT = "â™ž";
+	public final static String W_PAWN = "â™™";
+	public final static String B_PAWN = "â™Ÿ";
+	
+	// Constants for the class Pawn
+	public final static int B_MID_ROW = 3;
+	public final static int W_MID_ROW = 4;
+	public final static int B_DIRECTION_ROW = -1;
+	public final static int W_DIRECTION_ROW = 1;
+	
+	public final static int FIRST_ROW = 0;
+	public final static int LAST_ROW = 7;
 	
 }
