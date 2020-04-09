5d2c5e8ec6f4af4e59ae814362f67db8a142e3b7
Domenico Dell'Olio <zeito99@gmail.com>
2020-04-09 16:44:53 +0200

    Aggiunta classe King

---
+++ src/main/java/it/uniba/game/pieces/King.java
@@ -0,0 +1,29 @@
+package it.uniba.game.pieces;
+
+public final class King extends Piece {
+	//Sub-class of Piece representing the king
+	
+	//new Attributes to be added
+	
+	//Methods
+	public King(Color col) {
+		
+		super(col);
+		if (col == Piece.Color.BLACK) {
+			symbol="♚";
+			
+		} else {
+			symbol="♔";
+			
+		}
+		//eventually there will be introduced other inits
+		
+	}
+	
+	public it.uniba.game.Coordinates reverseMove(it.uniba.game.Coordinates endingPos){
+		
+		return new it.uniba.game.Coordinates(-1,-1);
+		//it returns an invalid value since it can't be moved for now
+	}
+
+}
