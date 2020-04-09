cb3533aa592109649ed30faae41ec27c17101ed5
Domenico Dell'Olio <zeito99@gmail.com>
2020-04-09 18:40:04 +0200

    Creata la classe Costanti e aggiunti i simboli dei pezzi

---
+++ src/main/java/it/uniba/game/Constants.java
@@ -0,0 +1,22 @@
+package it.uniba.game;
+
+
+
+final class Constants {
+	/*
+	 * Class containing the constant values used in the application
+	 */
+	final static String W_KING = "♔";
+	final static String B_KING = "♚";
+	final static String W_QUEEN = "♕";
+	final static String B_QUEEN = "♛";
+	final static String W_BISHOP = "♗";
+	final static String B_BISHOP = "♝";
+	final static String W_ROOK = "♖";
+	final static String B_ROOK = "♜";
+	final static String W_KNIGHT = "♘";
+	final static String B_KNIGHT = "♞";
+	final static String W_PAWN = "♙";
+	final static String B_PAWN = "♟";
+	
+}
