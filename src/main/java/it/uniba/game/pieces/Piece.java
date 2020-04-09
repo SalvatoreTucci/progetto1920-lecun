fd2b594e37880e6a426d63ed7378660a4ac414e7
llamandtea <lamantea.giuseppe@gmail.com>
2020-04-09 10:02:09 +0200

    Crea la classe astratta Piece

---
+++ src/main/java/it/uniba/game/pieces/Piece.java
@@ -0,0 +1,32 @@
+package pieces;
+
+public abstract class Piece{
+    /*
+    Abstract class representing a generic chess piece
+     */
+
+    enum Color {
+        // Enumerated type used to represent the color of the card
+        WHITE,
+        BLACK;
+    }
+
+    // Attributes
+    Color pieceColor;
+    String symbol;
+
+    // Methods
+    public Piece(Color col) {
+        pieceColor = col;
+    }
+
+    public Color getColor() {
+        return pieceColor;
+    }
+    public String toString() {
+        return symbol;
+    }
+
+    public Coordinates reverseMove(Coordinates endingPos);
+
+}
