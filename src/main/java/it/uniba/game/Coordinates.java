ef967ac649e4d7d5ecdff51ac02bea1484edfe6e
llamandtea <lamantea.giuseppe@gmail.com>
2020-04-09 10:56:37 +0200

    Crea la classe Coordinates

---
+++ src/main/java/it/uniba/game/Coordinates.java
@@ -0,0 +1,52 @@
+package game;
+
+public class Coordinates{
+    // Class used to represent a position in the chessboard
+
+    // Attributes
+    int row;
+    int column;
+
+    // Methods
+    public Coordinates(int x, int y) {
+
+        setRow(x);
+        setColumn(y);
+    }
+
+    public int getRow() {
+
+        return row;
+    }
+
+    public int getColumn() {
+
+        return column;
+    }
+
+    public void setRow(int x){
+
+        const int MIN_ROW = 0;
+        const int MAX_ROW = 7;
+
+        if(x >= MIN_ROW & x <= MAX_ROW)
+            row = x;
+        /*
+        else
+            aggiungere exception
+         */
+    }
+
+    public void setColumn(int y){
+
+        const int MIN_COLUMN = 0;
+        const int MAX_COLUMN = 7;
+
+        if(y >= MIN_COLUMN & y <= MAX_COLUMN)
+            column = y;
+        /*
+        else
+            aggiungere exception
+         */
+    }
+}
