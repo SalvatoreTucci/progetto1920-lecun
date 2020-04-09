3c93cef125142ee140c34f6bf21feb29e51e77b7
unknown <giovannidelvecchio4@gmail.com>
2020-04-09 15:26:53 +0200

    Aggiunta la classe Square

---
+++ src/main/java/it/uniba/game/board/Square.java
@@ -0,0 +1,42 @@
+package it.uniba.game.board;
+import it.uniba.game.pieces.Piece;
+
+public class Square {
+	/* 
+	 * This class represents the single square of the chessboard,
+	 * where pieces are placed
+	*/
+	
+	//Attributes
+	private Piece placed;
+	
+	
+	//Constructor
+	public Square(Piece toPlace) {
+		
+		placed = toPlace;
+	}
+	
+	//Methods
+	public Piece getPiece() {
+		
+		return placed;
+	}
+	
+	void setPiece(Piece toPlace) {
+		
+		placed = toPlace;
+	}
+	
+	
+	public boolean isOccupied() {
+		
+		if(getPiece() == null) {
+			
+			return false;
+		} else {
+			
+			return true;
+		}
+	} 
+}
