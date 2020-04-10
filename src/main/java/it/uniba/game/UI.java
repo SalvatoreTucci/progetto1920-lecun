c8c512f580b8b67561238b2e72c309ecb5e0acd6
Domenico Dell'Olio <zeito99@gmail.com>
2020-04-10 16:29:15 +0200

    aggiunto il metodo printBoard e riorganizzato l'ordine

--- src/main/java/it/uniba/game/UI.java
+++ src/main/java/it/uniba/game/UI.java
@@ -12,35 +12,7 @@ public class UI {
 	private boolean quitGame;
 	
 	//Methods
-	/*
-	 * triggered by the command "play", inits the currentMatch variable
-	 */
-	void startGame() {
-		
-		System.out.println(WELCOME);
-		currentMatch=new Match();
-	}
-	
-	/*
-	 * asks the user whether to quit or not the whole application. If
-	 * the response isn't affirmative nor negative, the method keeps
-	 * asking for a response.
-	 */
-	void quit() {
-	
-		System.out.println(QUIT_MESSAGE);
-		Scanner keyboard = new Scanner(System.in);
-		String read;
-		do {
-			read = keyboard.nextLine();
-			if(read.contains(YES)) {
-				quitGame=true;
-			} else if(!read.contains(NO)) {
-				System.out.println(INCORRECT_QUIT_RESPONSE);
-			}
-		} while (!read.contains(YES) && !read.contains(NO));
-	}
-	
+
 	/*
 	 * returns the required status of the application (true = to be closed,
 	 *  false = to be kept open)
@@ -49,15 +21,10 @@ public class UI {
 		
 		return quitGame;
 	}
-	
+
 	/*
-	 * prints a list of commands on-screen
+	 * parses the command read from the keyboard and executes the correspondent routine
 	 */
-	void printHelp() {
-		
-		System.out.println(HELP);
-	}
-	
 	public void parseCommand(String toParse) {
 		
 		switch (toParse) {
@@ -75,6 +42,11 @@ public class UI {
 		}
 	}
 	
+	/*
+	 * Prosecution of parseCommand, delegated to handling commands during a match.
+	 * It checks whether a match is open before executing the respecting routine, otherwise
+	 * it will display an error.
+	 */
 	private void handleInGameCommand(String toParse) {
 		
 		if (currentMatch != null) {
@@ -96,5 +68,55 @@ public class UI {
 		}
 	
 	}
+	
+	/*
+	 * triggered by the command "play", print a welcome message 
+	 * and inits the currentMatch variable
+	 */
+	private void startGame() {
+		
+		System.out.println(WELCOME);
+		currentMatch=new Match();
+	}
+	
+	/*
+	 * asks the user whether to quit or not the whole application. If
+	 * the response isn't affirmative nor negative, the method keeps
+	 * asking for a response.
+	 */
+	private void quit() {
+	
+		System.out.println(QUIT_MESSAGE);
+		Scanner keyboard = new Scanner(System.in);
+		String read;
+		do {
+			read = keyboard.nextLine();
+			if(read.contains(YES)) {
+				quitGame=true;
+			} else if(!read.contains(NO)) {
+				System.out.println(INCORRECT_QUIT_RESPONSE);
+			}
+		} while (!read.contains(YES) && !read.contains(NO));
+	}
+	
+	
+	/*
+	 * prints a list of commands on-screen
+	 */
+	private void printHelp() {
+		
+		System.out.println(HELP);
+	}
+	
+	/*
+	 * on command board, it prints the current state of the board on screen   
+	 */
+	private void printBoard() {
+		
+		System.out.println(currentMatch.getPrintableBoard() + "\n");
+	}
+	
+
+	
 
 }
