795d0c349ec9af8d9f4fb1e05400dc4ea95d287f
Raffo99 <dis.raffaele99@gmail.com>
2020-04-11 19:49:23 +0200

    Modificato il metodo reverseMove nella classe Piece

--- src/main/java/it/uniba/game/pieces/Piece.java
+++ src/main/java/it/uniba/game/pieces/Piece.java
@@ -1,5 +1,7 @@
 package it.uniba.game.pieces;
 import java.util.Vector;
+import it.uniba.game.Coordinates;
+import it.uniba.game.Move;
 
 public abstract class Piece{
     /*
@@ -32,6 +34,6 @@ public abstract class Piece{
         return symbol;
     }
 
-    abstract public Vector<it.uniba.game.Coordinates> reverseMove(it.uniba.game.Coordinates endingPos);
+    abstract public Vector<Coordinates> reverseMove(Move endingPos);
 
 }
