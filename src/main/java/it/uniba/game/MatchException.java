ca345cee1024468fe9faf15031c33c6186c86564
Domenico Dell'Olio <zeito99@gmail.com>
2020-04-15 18:43:34 +0200

    Sistemato lo stile nella classe MatchException

--- src/main/java/it/uniba/game/MatchException.java
+++ src/main/java/it/uniba/game/MatchException.java
@@ -1,10 +1,13 @@
 package it.uniba.game;
 
-class MatchException extends Exception{
+
+
+class MatchException extends Exception {
 	
 	// Exception class threw from the class Move
 	
 	public MatchException(String errorMessage) {
+		
 		super(errorMessage);
 	}
 	
