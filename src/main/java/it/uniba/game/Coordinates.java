b20b80887ac10bac11b8be60df3e232d3414432b
Domenico Dell'Olio <zeito99@gmail.com>
2020-04-15 18:20:31 +0200

    Sistemato lo stile nella classe Coordinates

--- src/main/java/it/uniba/game/Coordinates.java
+++ src/main/java/it/uniba/game/Coordinates.java
@@ -1,7 +1,7 @@
 package it.uniba.game;
 
-public class Coordinates{
-    // Class used to represent a position in the chessboard
+public class Coordinates {
+    // Class used to represent a position in the ChessBoard
 
     // Attributes
     private int row;
@@ -24,13 +24,14 @@ public class Coordinates{
         return column;
     }
 
-    public void setRow(int y){
+    public void setRow(int y) {
 
         row = y;
     }
 
-    public void setColumn(int x){
+    public void setColumn(int x) {
 
         column = x;
     }
+    
 }
