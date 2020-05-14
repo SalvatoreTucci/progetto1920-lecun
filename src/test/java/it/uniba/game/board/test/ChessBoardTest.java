8d0a9e3ff39eed139f8677400c2c512c7ffa448f
Domenico Dell'Olio <zeito99@gmail.com>
2020-05-14 20:41:42 +0200

    corretto lo stile delle spaziature

--- src/test/java/it/uniba/game/board/test/ChessBoardTest.java
+++ src/test/java/it/uniba/game/board/test/ChessBoardTest.java
@@ -22,23 +22,26 @@ public class ChessBoardTest {
 	private static Pawn dummyPiece;
 	
 	@BeforeAll
-	static public void PieceInit(){
+	static public void PieceInit() {
+		
 		dummyPiece = new Pawn(Color.WHITE);
 	}
 	
 	@BeforeEach
-	public void ChessBoardInit(){
+	public void ChessBoardInit() {
+		
 		cb = new ChessBoard(); 
 	}
 	
 	@Test
 	public void setMoveCorrectTest() {
 		
-		Coordinates start = new Coordinates(0,Constants.WHITE_PAWN_ROW);
-		Coordinates end = new Coordinates(0,5); 
+		Coordinates start = new Coordinates(0, Constants.WHITE_PAWN_ROW);
+		Coordinates end = new Coordinates(0, 5); 
 		
 		cb.setMove(new Move(dummyPiece, start, end, false));
-		assertAll(() ->{assertNull(cb.getSquare(start).getPiece());
+		
+		assertAll(() -> {assertNull(cb.getSquare(start).getPiece());
 						assertEquals(dummyPiece, cb.getSquare(end).getPiece());
 						});	
 	}
@@ -53,10 +56,11 @@ public class ChessBoardTest {
 	@Test
 	public void setMovePieceNullTest() {
 		
-		Coordinates start = new Coordinates(1,Constants.WHITE_PAWN_ROW);
-		Coordinates end = new Coordinates(1,5); 
+		Coordinates start = new Coordinates(1, Constants.WHITE_PAWN_ROW);
+		Coordinates end = new Coordinates(1, 5); 
 
 		cb.setMove(new Move(null, start, end, false));
+		
 		assertAll(() -> {assertNull(cb.getSquare(start).getPiece());
 						assertNull(cb.getSquare(end).getPiece());
 						});	
@@ -65,92 +69,94 @@ public class ChessBoardTest {
 	@Test
 	public void setMoveStartNullTest() {
 		
-		Coordinates end = new Coordinates(0,5); 
+		Coordinates end = new Coordinates(0, 5); 
 		
-		assertThrows(NullPointerException.class,() -> cb.setMove(new Move(dummyPiece, null, end, false)));
+		assertThrows(NullPointerException.class, () -> cb.setMove(new Move(dummyPiece, null, end, false)));
 
 	}
 	
 	@Test
 	public void setMoveEndNullTest() {
 		
-		Coordinates start = new Coordinates(4,Constants.WHITE_PAWN_ROW); 
+		Coordinates start = new Coordinates(4, Constants.WHITE_PAWN_ROW); 
 
-		assertThrows(NullPointerException.class,() -> cb.setMove(new Move(dummyPiece, start, null, false)));
+		assertThrows(NullPointerException.class, () -> cb.setMove(new Move(dummyPiece, start, null, false)));
 
 	}
 	
 	@Test
 	public void setMoveWrongStartColumnTest() {
 		
-		Coordinates start = new Coordinates(-1,Constants.WHITE_PAWN_ROW);
-		Coordinates end = new Coordinates(0,5); 
+		Coordinates start = new Coordinates(-1, Constants.WHITE_PAWN_ROW);
+		Coordinates end = new Coordinates(0, 5); 
 
-		assertThrows(IndexOutOfBoundsException.class,() -> cb.setMove(new Move(dummyPiece, start, end, false)));
+		assertThrows(IndexOutOfBoundsException.class, () -> cb.setMove(new Move(dummyPiece, start, end, false)));
 		
 	}
 	
 	@Test
 	public void setMoveWrongStartRowTest() {
 		
-		Coordinates start = new Coordinates(0,8);
-		Coordinates end = new Coordinates(0,5); 
+		Coordinates start = new Coordinates(0, 8);
+		Coordinates end = new Coordinates(0, 5); 
 		
-		assertThrows(IndexOutOfBoundsException.class,() -> cb.setMove(new Move(dummyPiece, start, end, false)));
+		assertThrows(IndexOutOfBoundsException.class, () -> cb.setMove(new Move(dummyPiece, start, end, false)));
 		
 	}
 	
 	@Test
 	public void setMoveWrongStartBothTest() {
 		
-		Coordinates start = new Coordinates(8,-1);
-		Coordinates end = new Coordinates(0,5); 
+		Coordinates start = new Coordinates(8, -1);
+		Coordinates end = new Coordinates(0, 5); 
 		
-		assertThrows(IndexOutOfBoundsException.class,() -> cb.setMove(new Move(dummyPiece, start, end, false)));
+		assertThrows(IndexOutOfBoundsException.class, () -> cb.setMove(new Move(dummyPiece, start, end, false)));
 		
 	}
 	
 	@Test
 	public void setMoveWrongEndRowTest() {
 		
-		Coordinates start = new Coordinates(0,Constants.WHITE_PAWN_ROW);
-		Coordinates end = new Coordinates(7,8); 
+		Coordinates start = new Coordinates(0, Constants.WHITE_PAWN_ROW);
+		Coordinates end = new Coordinates(7, 8); 
 
-		assertThrows(IndexOutOfBoundsException.class,() -> cb.setMove(new Move(dummyPiece, start, end, false)));
+		assertThrows(IndexOutOfBoundsException.class, () -> cb.setMove(new Move(dummyPiece, start, end, false)));
 		
 	}
 	
 	@Test
 	public void setMoveWrongEndColumnTest() {
 		
-		Coordinates start = new Coordinates(0,7);
-		Coordinates end = new Coordinates(-1,7); 
+		Coordinates start = new Coordinates(0, 7);
+		Coordinates end = new Coordinates(-1, 7); 
 		
-		assertThrows(IndexOutOfBoundsException.class,() -> cb.setMove(new Move(dummyPiece, start, end, false)));
+		assertThrows(IndexOutOfBoundsException.class, () -> cb.setMove(new Move(dummyPiece, start, end, false)));
 		
 	}
 	
 	@Test
 	public void setMoveWrongEndBothTest() {
 		
-		Coordinates start = new Coordinates(0,Constants.WHITE_PAWN_ROW);
-		Coordinates end = new Coordinates(8,-1); 
+		Coordinates start = new Coordinates(0, Constants.WHITE_PAWN_ROW);
+		Coordinates end = new Coordinates(8, -1); 
 		
-		assertThrows(IndexOutOfBoundsException.class,() -> cb.setMove(new Move(dummyPiece, start, end, false)));
+		assertThrows(IndexOutOfBoundsException.class, () -> cb.setMove(new Move(dummyPiece, start, end, false)));
 	}
 	
 	@Test
 	public void setMoveEnPassantWhiteCorrectTest() {
 		
-		Coordinates whitePawnStart = new Coordinates(1,3);
-		Coordinates whitePawnEnd = new Coordinates(0,2);
-		Coordinates blackPawnEnd = new Coordinates(0,3);
+		Coordinates whitePawnStart = new Coordinates(1, 3);
+		Coordinates whitePawnEnd = new Coordinates(0, 2);
+		Coordinates blackPawnEnd = new Coordinates(0, 3);
+		Move enPass =new Move(dummyPiece, whitePawnStart, whitePawnEnd, true);
+		enPass.setEnPassant();
+		
 		cb.setMove(new Move(new Pawn(Piece.Color.WHITE), new Coordinates(1,Constants.WHITE_PAWN_ROW),
 				whitePawnStart, false));
 		cb.setMove(new Move(new Pawn(Piece.Color.BLACK), new Coordinates(0,Constants.BLACK_PAWN_ROW),
 				blackPawnEnd, false));
-		Move enPass =new Move(dummyPiece, whitePawnStart, whitePawnEnd, true);
-		enPass.setEnPassant();
+		
 		cb.setMove(enPass);
 		assertAll(() -> {assertEquals(dummyPiece, cb.getSquare(whitePawnEnd).getPiece());
 						assertNull(cb.getSquare(blackPawnEnd).getPiece());
@@ -161,16 +167,18 @@ public class ChessBoardTest {
 	@Test
 	public void setMoveEnPassantBlackCorrectTest() {
 		
-		Coordinates blackPawnStart = new Coordinates(6,4);
-		Coordinates blackPawnEnd = new Coordinates(7,5);
-		Coordinates whitePawnEnd = new Coordinates(7,4);
+		Coordinates blackPawnStart = new Coordinates(6, 4);
+		Coordinates blackPawnEnd = new Coordinates(7, 5);
+		Coordinates whitePawnEnd = new Coordinates(7, 4);
+		Pawn blackP = new Pawn(Piece.Color.BLACK);
+		Move enPass =new Move(blackP, blackPawnStart, blackPawnEnd, true);
+		enPass.setEnPassant();
+		
 		cb.setMove(new Move(new Pawn(Piece.Color.WHITE), new Coordinates(1,Constants.WHITE_PAWN_ROW),
 				whitePawnEnd, false));
 		cb.setMove(new Move(new Pawn(Piece.Color.BLACK), new Coordinates(0,Constants.BLACK_PAWN_ROW),
 				blackPawnStart, false));
-		Pawn blackP = new Pawn(Piece.Color.BLACK);
-		Move enPass =new Move(blackP, blackPawnStart, blackPawnEnd, true);
-		enPass.setEnPassant();
+		
 		cb.setMove(enPass);
 		assertAll(() -> {assertEquals(blackP, cb.getSquare(blackPawnEnd).getPiece());
 						assertNull(cb.getSquare(whitePawnEnd).getPiece());
