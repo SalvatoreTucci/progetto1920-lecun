83fad10d34ecb4164205cdad941d14bff8f270a4
unknown <giovannidelvecchio4@gmail.com>
2020-05-15 16:21:32 +0200

    Aggiunti i casi di test mancanti

--- src/test/java/it/uniba/game/pieces/test/KnightTest.java
+++ src/test/java/it/uniba/game/pieces/test/KnightTest.java
@@ -2,6 +2,7 @@ package it.uniba.game.pieces.test;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
@@ -183,5 +184,65 @@ public class KnightTest {
 		
 		Object[] arrayToCheck = toTest.reverseMove(targetMove).toArray();
 		assertArrayEquals(possibleCoord, arrayToCheck);
+	}
+	
+	@Test
+	public void reverseMoveNullMoveTest() {	
+
+		Knight toTest = new Knight(Piece.Color.WHITE);
+		assertThrows(NullPointerException.class, () ->toTest.reverseMove(null));
+	}
+
+	@Test
+	public void reverseMoveNullPieceTest() {
+
+		Knight toTest = new Knight(Piece.Color.WHITE);
+		Move targetMove = new Move(null, Constants.EMPTY_COORD, new Coordinates(3,4), false);
+		Coordinates[] possibleCoord = {
+				
+			new Coordinates(5, 5),
+			new Coordinates(1, 5),
+			new Coordinates(4, 6),
+			new Coordinates(2, 6),
+			new Coordinates(5, 3),
+			new Coordinates(1, 3),
+			new Coordinates(4, 2),
+			new Coordinates(2, 2)
+		};
+		
+		Object[] arrayToCheck = toTest.reverseMove(targetMove).toArray();
+		assertArrayEquals(possibleCoord, arrayToCheck);
+
+	}
+
+	@Test
+	public void reverseMoveNullStartTest() {
+
+		Knight toTest = new Knight(Piece.Color.WHITE);
+		Move targetMove = new Move(toTest, null, new Coordinates(3,4), false);
+		Coordinates[] possibleCoord = {
+				
+			new Coordinates(5, 5),
+			new Coordinates(1, 5),
+			new Coordinates(4, 6),
+			new Coordinates(2, 6),
+			new Coordinates(5, 3),
+			new Coordinates(1, 3),
+			new Coordinates(4, 2),
+			new Coordinates(2, 2)
+		};
+		
+		Object[] arrayToCheck = toTest.reverseMove(targetMove).toArray();
+		assertArrayEquals(possibleCoord, arrayToCheck);
+	}
+
+	@Test
+	public void reverseMoveEndMoveTest() {
+
+		Knight toTest = new Knight(Piece.Color.WHITE);
+		
+		assertThrows(NullPointerException.class, () ->toTest.reverseMove(new Move(toTest,
+				Constants.EMPTY_COORD,
+				null, false)));
 	}
 }
