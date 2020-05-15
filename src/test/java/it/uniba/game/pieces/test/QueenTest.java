1f1abb19e76afdd7f9dde655f93a9aa3d00f73ba
llamandtea <lamantea.giuseppe@gmail.com>
2020-05-15 16:42:19 +0200

    Crea i casi di test per la classe Queen

---
+++ src/test/java/it/uniba/game/pieces/test/QueenTest.java
@@ -0,0 +1,109 @@
+package it.uniba.game.pieces.test;
+
+import it.uniba.game.Constants;
+import it.uniba.game.pieces.Piece.Color;
+import it.uniba.game.pieces.Queen;
+import it.uniba.game.pieces.Rook;
+import it.uniba.game.pieces.Bishop;
+import it.uniba.game.Move;
+import it.uniba.game.Coordinates;
+import java.util.LinkedList;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class QueenTest {
+
+	private final Queen testQueen = new Queen(Color.BLACK);
+	
+	@Test
+	void constructorWhiteTest() {
+		
+		assertEquals(Constants.W_QUEEN, new Queen(Color.WHITE).toString());
+	}
+	
+	@Test
+	void constructorBlackTest() {
+		
+		assertEquals(Constants.B_QUEEN, testQueen.toString());
+	}
+	
+	@Test
+	void reverseMoveCenterTest() {
+		
+		/*
+		 * tests the normal behaviour of the reverseMove method for the Queen, starting from coordinate (3, 3)
+		 * to build the list of coordinates to check against the output of the method reverseMove, it is used
+		 * the reverseMove method from Bishop and Rook
+		 */
+		Coordinates testCoord = new Coordinates(3, 3);
+		LinkedList<Coordinates> coordinatesToTest = testQueen.reverseMove(new Move(testQueen, null, testCoord, false));
+		Move testMove = new Move(null, null, testCoord, false);
+		LinkedList<Coordinates> possibleSquares = Rook.reverseRookMove(testMove);
+		possibleSquares.addAll(Bishop.reverseBishopMove(testMove));
+		
+		assertEquals(possibleSquares, coordinatesToTest);
+	}
+	
+	@Test
+	void reverseMoveTopLeftTest() {
+		
+		Coordinates testCoord = new Coordinates(0, 0);
+		LinkedList<Coordinates> coordinatesToTest = testQueen.reverseMove(new Move(testQueen, null, testCoord, false));
+		Move testMove = new Move(null, null, testCoord, false);
+		LinkedList<Coordinates> possibleSquares = Rook.reverseRookMove(testMove);
+		possibleSquares.addAll(Bishop.reverseBishopMove(testMove));
+		
+		assertEquals(possibleSquares, coordinatesToTest);		
+	}
+	
+	@Test
+	void reverseMoveTopRightTest() {
+		
+		Coordinates testCoord = new Coordinates(7, 0);
+		LinkedList<Coordinates> coordinatesToTest = testQueen.reverseMove(new Move(testQueen, null, testCoord, false));
+		Move testMove = new Move(null, null, testCoord, false);
+		LinkedList<Coordinates> possibleSquares = Rook.reverseRookMove(testMove);
+		possibleSquares.addAll(Bishop.reverseBishopMove(testMove));
+		
+		assertEquals(possibleSquares, coordinatesToTest);
+	}
+	
+	@Test
+	void reverseMoveBottomLeftTest() {
+		
+		Coordinates testCoord = new Coordinates(0, 7);
+		LinkedList<Coordinates> coordinatesToTest = testQueen.reverseMove(new Move(testQueen, null, testCoord, false));
+		Move testMove = new Move(null, null, testCoord, false);
+		LinkedList<Coordinates> possibleSquares = Rook.reverseRookMove(testMove);
+		possibleSquares.addAll(Bishop.reverseBishopMove(testMove));
+		
+		assertEquals(possibleSquares, coordinatesToTest);
+	}
+
+	@Test
+	void reverseMoveBottomRightTest() {
+		
+		Coordinates testCoord = new Coordinates(7, 7);
+		LinkedList<Coordinates> coordinatesToTest = testQueen.reverseMove(new Move(testQueen, null, testCoord, false));
+		Move testMove = new Move(null, null, testCoord, false);
+		LinkedList<Coordinates> possibleSquares = Rook.reverseRookMove(testMove);
+		possibleSquares.addAll(Bishop.reverseBishopMove(testMove));
+		
+		assertEquals(possibleSquares, coordinatesToTest);
+	}
+	
+	@Test
+	void reverseMoveNegativeTest() {
+		
+		Coordinates testCoord = new Coordinates(-1, -1);
+		assertEquals(new LinkedList<Coordinates>(), testQueen.reverseMove(new Move(testQueen, null, testCoord, false)));
+	}
+	
+	@Test
+	void reverseMoveOutOfBoundTest() {
+		
+		Coordinates testCoord = new Coordinates(9, 8);
+		assertEquals(new LinkedList<Coordinates>(), testQueen.reverseMove(new Move(testQueen, null, testCoord, false)));
+	}
+}
