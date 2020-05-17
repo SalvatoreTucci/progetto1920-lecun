95b06ad9ba3afcd1d90d3bcb59b4b84b01197749
Domenico Dell'Olio <zeito99@gmail.com>
2020-05-17 15:51:20 +0200

    Risolti problemi checkStyle in King

--- src/main/java/it/uniba/game/pieces/King.java
+++ src/main/java/it/uniba/game/pieces/King.java
@@ -11,7 +11,7 @@ import it.uniba.game.Move;
  * 
  * Responsibilities: <br>
  * 
- * 	Knows: 
+ * 	Knows:
  * 		<ul>
  * 		<li>The symbol (and so the color) of the instance of
  * 			the King</li>
@@ -21,70 +21,75 @@ import it.uniba.game.Move;
  * 	Does:
  * 		<ul>
  * 		<li>Generates a list of coordinates where a King could
- * 			possibly be found, given the 
+ * 			possibly be found, given the
  * 			target position</li>
  * 		<li>Reports if an instance of King has been moved</li>
  * 		</ul>
  * 
 */
 public final class King extends Piece {
-	
+
 	private boolean moved = false;
-	
-	//Constructor
-	public King(Color col) {
-		
+
+	// Constructor
+	public King(final Color col) {
+
 		super(col);
-		
+
 		if (col == Color.BLACK) {
-			
+
 			this.setSymbol(Constants.B_KING);
 		} else {
-			
+
 			this.setSymbol(Constants.W_KING);
 		}
-		//eventually there will be introduced other inits
+
 	}
-	
-	public LinkedList<Coordinates> reverseMove(Move target){
-		
+
+	public LinkedList<Coordinates> reverseMove(final Move target) {
+
 		LinkedList<Coordinates> possibleSquares = new LinkedList<Coordinates>();
-		
+
 		if (checkOutOfBound(target)) {
-			
+
 			return possibleSquares;
 		}
-		
+
 		int endColumn = target.getEndingPos().getColumn();
 		int endRow    = target.getEndingPos().getRow();
-		
-		for(int i = Constants.START_ROW_KING_REVERSE; i <= Constants.MAX_ROW_KING_REVERSE; i++) {
-			
-			for(int j = Constants.START_COL_KING_REVERSE; j <= Constants.MAX_COL_KING_REVERSE; j++) {
-				
-				if(endRow + i > Constants.LAST_ROW || endRow + i < Constants.FIRST_ROW
-						|| endColumn + j > Constants.LAST_COLUMN || endColumn + j < Constants.FIRST_COLUMN
+
+		for (int i = Constants.START_ROW_KING_REVERSE; i <= Constants.MAX_ROW_KING_REVERSE; i++) {
+
+			for (int j = Constants.START_COL_KING_REVERSE; j <= Constants.MAX_COL_KING_REVERSE; j++) {
+
+				if (endRow + i > Constants.LAST_ROW
+						|| endRow + i < Constants.FIRST_ROW
+						|| endColumn + j > Constants.LAST_COLUMN
+						|| endColumn + j < Constants.FIRST_COLUMN
 						|| (i == 0 && j == 0)) {
-					
+
 					continue;
 				}
-				
+
 				possibleSquares.add(new Coordinates(endColumn + j, endRow + i));
+
 			}
-			
+
 		}
 
+
 		return possibleSquares;
+
 	}
 
 	public boolean isMoved() {
-		
+
 		return moved;
 	}
 
-	public void setMoved(boolean moved) {
-		
-		this.moved = moved;
+	public void setMoved(final boolean hasMoved) {
+
+		this.moved = hasMoved;
 	}
 
 }
